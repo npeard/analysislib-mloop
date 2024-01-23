1bf9380ceedc8c178547d50df6d8b2c294dd146f
Spielman Lab <ian.spielman@nist.gov>
2024-01-23 14:25:56 -0500

    Got it working.

--- mloop_lyse.py
+++ mloop_lyse.py
@@ -1,5 +1,10 @@
 import lyse
-import numpy as np
-import mloop_multishot
+import os
+import analysislib.common.mloop.mloop_multishot as mloop_multishot
 
-mloop_multishot.run_singleshot_multishot()
+if __name__ == '__main__':
+    config_file = os.path.join(os.path.dirname(__file__), "mloop_config.toml")
+    mloop_multishot.run_singleshot_multishot(config_file)
+else:
+    # I don't know how we find ourselves here, but it occurs.
+    print("mloop_lyse not in main")
