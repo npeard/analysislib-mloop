4d65834c8d199126b8a96f5f799df9c8d574384d
spielman <spielman@umd.edu>
2024-02-05 11:45:56 -0500

    Cleanup based on most recent commit to M-LOOP

--- mloop_controller.py
+++ mloop_controller.py
@@ -1,7 +1,5 @@
 from mloop.controllers import GaussianProcessController, ControllerInterrupt
-from . import mloop_learner
 import mloop.utilities as mlu
-import logging
 import traceback
 
 class LoopController(GaussianProcessController):
@@ -21,49 +19,6 @@ class LoopController(GaussianProcessController):
 
         self.num_buffered_runs = interface.num_buffered_runs
 
-        # This is the biggest change
-        self.last_out_params = mlu.queue.Queue()
-        self.cost_obtained = False
-
-    def _put_params_and_out_dict(self, params, param_type=None, **kwargs):
-        '''
-        Send parameters to queue with optional additional keyword arguments.
-
-        This method also saves sent variables in appropriate storage arrays.
-
-        Args:
-            params (array): Array of values to be experimentally tested.
-            param_type (Optional, str): The learner type which generated the
-                parameter values. Because some learners use other learners as
-                trainers, the parameter type can be different for different
-                iterations during a given optimization. This value will be
-                stored in `self.out_type` and in the `out_type` list in the
-                controller archive. If `None`, then it will be set to
-                `self.learner.OUT_TYPE`. Default `None`.
-        Keyword Args:
-            **kwargs: Any additional keyword arguments will be stored in
-                `self.out_extras` and in the `out_extras` list in the controller
-                archive.
-        '''
-        # Set default values if needed.
-        if param_type is None:
-            param_type = self.learner.OUT_TYPE
-
-        # Do one last check to ensure parameter values are within the allowed
-        # limits before sending those values to the interface.
-        self.log.info('enforcing boundaries')
-        params = self._enforce_boundaries(params)
-
-        # Send the parameters to the interface and update various attributes.
-        out_dict = {'params':params}
-        out_dict.update(kwargs)
-        self.params_out_queue.put(out_dict)
-        self.num_out_params += 1
-        self.last_out_params.put(params) # IBS: the only change here
-        self.out_params.append(params)
-        self.out_extras.append(kwargs)
-        self.out_type.append(param_type)
-
     def _get_cost_and_in_dict(self):
         '''
         Get cost, uncertainty, parameters, bad and extra data from experiment.
@@ -77,7 +32,6 @@ class LoopController(GaussianProcessController):
         controller here so that the error can be re-raised in the controller's
         thread (note that the interface runs in a separate thread).
         '''
-        self.cost_obtained = False
         while True:
             try:
                 in_dict = self.costs_in_queue.get(True, self.controller_wait)
@@ -105,53 +59,48 @@ class LoopController(GaussianProcessController):
                     raise err
             else:
                 # Got a cost dict, so exit this while loop.
-                self.cost_obtained = True
-
                 break
 
-        # The only way to get here should be to have self.cost_obtained = True, but check anyway
-        if self.cost_obtained:
-            self.num_in_costs += 1
-            self.num_last_best_cost += 1
+        self.num_in_costs += 1
+        self.num_last_best_cost += 1
 
-            if not ('cost' in in_dict) and (not ('bad' in in_dict) or not in_dict['bad']):
-                self.log.error('You must provide at least the key cost or the key bad with True.')
-                raise ValueError
-            try:
-                self.curr_cost = float(in_dict.pop('cost',float('nan')))
-                self.curr_uncer = float(in_dict.pop('uncer',0))
-                self.curr_bad = bool(in_dict.pop('bad',False))
-                self.curr_extras = in_dict
-            except ValueError:
-                self.log.error('One of the values you provided in the cost dict could not be converted into the right type.')
-                raise
-            if self.curr_bad and ('cost' in in_dict):
-                self.log.warning('The cost provided with the bad run will be saved, but not used by the learners.')
+        if not ('cost' in in_dict) and (not ('bad' in in_dict) or not in_dict['bad']):
+            self.log.error('You must provide at least the key cost or the key bad with True.')
+            raise ValueError
+        try:
+            self.curr_cost = float(in_dict.pop('cost',float('nan')))
+            self.curr_uncer = float(in_dict.pop('uncer',0))
+            self.curr_bad = bool(in_dict.pop('bad',False))
+            self.curr_extras = in_dict
+        except ValueError:
+            self.log.error('One of the values you provided in the cost dict could not be converted into the right type.')
+            raise
+        if self.curr_bad and ('cost' in in_dict):
+            self.log.warning('The cost provided with the bad run will be saved, but not used by the learners.')
 
-            self.in_costs.append(self.curr_cost)
-            self.in_uncers.append(self.curr_uncer)
-            self.in_bads.append(self.curr_bad)
-            self.in_extras.append(self.curr_extras)
-            self.curr_params = self.last_out_params.get() # IBS: change to queue here
-            if self.curr_cost < self.best_cost: 
-                self.best_cost = self.curr_cost
-                self.best_uncer = self.curr_uncer
-                self.best_index =  self.num_in_costs - 1  # -1 for zero-indexing.
-                self.best_params = self.curr_params
-                self.best_in_extras = self.curr_extras
-                self.num_last_best_cost = 0
-            if self.curr_bad:
-                self.log.info('bad run')
-            else:
-                self.log.info('cost ' + str(self.curr_cost) + ' +/- ' + str(self.curr_uncer))
-
-            # IBS: composing base Controller class with MachineLearnerController manually
-            self.log.debug('sending data to machine learner')
-            self.ml_learner_costs_queue.put(
-                (self.curr_params, self.curr_cost, self.curr_uncer, self.curr_bad)
-            )
+        self.in_costs.append(self.curr_cost)
+        self.in_uncers.append(self.curr_uncer)
+        self.in_bads.append(self.curr_bad)
+        self.in_extras.append(self.curr_extras)
+        self.curr_params = self.last_out_params.get() # IBS: change to queue here
+        if self.curr_cost < self.best_cost: 
+            self.best_cost = self.curr_cost
+            self.best_uncer = self.curr_uncer
+            self.best_index =  self.num_in_costs - 1  # -1 for zero-indexing.
+            self.best_params = self.curr_params
+            self.best_in_extras = self.curr_extras
+            self.num_last_best_cost = 0
+        if self.curr_bad:
+            self.log.info('bad run')
         else:
-            self.log.debug('in unreachable code')
+            self.log.info('cost ' + str(self.curr_cost) + ' +/- ' + str(self.curr_uncer))
+
+        # IBS: composing base Controller class with MachineLearnerController manually
+        self.log.debug('sending data to machine learner')
+        self.ml_learner_costs_queue.put(
+            (self.curr_params, self.curr_cost, self.curr_uncer, self.curr_bad)
+        )
+
 
     def _optimization_routine(self):
         '''
